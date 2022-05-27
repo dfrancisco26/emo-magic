581c3a1de2112d468bc7055b9be2a1246e76a276
David Francisco <dfrancisco2677@gmail.com>
2022-05-26 17:45:56 -0700

    Cleaning up.

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,5 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { renderWorkshop } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -15,14 +14,5 @@ test('time to test a function', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
-});
-
-test ('workshop render', (expect) => {
-    const expected = '<div class="workshop"><img src="../assets/cry.jpg"><h2>Tearbending</h2><p>Like waterbending, but saltier!</p><ul><li>Bufo <br>Contact Info: Wiggle your fingers while thinking of toads.</li></ul></div>';
-
-
-    const actual = renderWorkshop();
-
     expect.equal(actual, expected);
 });
